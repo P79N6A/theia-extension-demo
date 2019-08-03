60f24fae407110255603b3d04a93aa0d87a20567
NekoTrek <creamidea@gmail.com>
2019-08-03 21:45:57 +0800

    chore: remove some files

--- packages/hello-world/lib/browser/menu-contribution.d.ts
+++
@@ -1,5 +0,0 @@
-import { MenuContribution, MenuModelRegistry } from '@theia/core';
-export declare class HelloWorldMenuContribution implements MenuContribution {
-    registerMenus(menus: MenuModelRegistry): void;
-}
-//# sourceMappingURL=menu-contribution.d.ts.map
